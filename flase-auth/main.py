988ad9cbbb1fd470fb5a1606040f5d56efeed390
ichebadu <jazzmaniche@gmai.com>
2025-02-15 04:44:29 +0100

    implement login, register and logout

--- flase-auth/main.py
+++ flase-auth/main.py
@@ -11,13 +11,18 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
-#Database Model
+#Database Model = single roll with the DB
 class User(db.Model):
     #class variables
-    id
-    username
-    password
+    id = db.Column(db.Integer, primary_key =True)
+    username = db.Column(db.String(25), unique = True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 #Routes
 @app.route("/")
@@ -25,6 +30,56 @@ def home():
     if "username" in session:
         return redirect(url_for('dashboard'))
     return render_template("index.html")
+
+#Login
+@app.route("/login", methods = ["POST"])
+def login ():
+
+    #collect info from form
+    username = request.form["username"]
+    password = request.form["password"]
+    user = User.query.filter_by(username = username).first()
+    if user and user.check_password(password):
+        session["username"] = username
+        return redirect(url_for("dashboard"))
+    else:
+        return render_template("index.html")
+     
+
+    #check if its int the db / login
+
+    #otherwise  show home page
+
+#Register
+@app.route("/register", methods = ["POST"])
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+    user = User.query.filter_by(username = username).first()
+    if user:
+        return render_template("index.html", error="user already exist")
+    else:
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        session["username"] = username
+        return redirect(url_for("dashboard"))
+
+
+#Dashboard
+@app.route("/dashboard")
+def dashboard():
+    if "username" in session:
+        return render_template("dashboard.html", username=session["username"])
+    return redirect(url_for("home"))
+
+#Logout
+@app.route("/logout")
+def logout():
+    session.pop("username",None)
+    return redirect(url_for("home"))
+
 
 
 if __name__ in "__main__":
